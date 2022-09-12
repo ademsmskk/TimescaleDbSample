853ad96d80a56e6170fdab67befbcd787fbc85dc
Adem Şimşek <ademx1907@gmail.com>
2022-09-12 09:28:39 +0300

    Add project files.

---
+++ Timescaledb-ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Timescaledb_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimescaleHelper ts = new TimescaleHelper();
+
+
+            ts.CheckDatabaseConnection();
+            ts.insertData();
+            ts.runQuery();
+            ts.CreateRelationalData();
+            ts.CreateHypertable();
+            ts.insertData2();
+            ts.RunQueryExample();
+
+
+
+
+           
+        }
+    }
+}
